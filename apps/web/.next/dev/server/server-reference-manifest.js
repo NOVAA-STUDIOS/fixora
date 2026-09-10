bd2d578e51d0ee61a75cb25fdea58936e8bc3b81
NOVAA-STUDIOS <novaastudioss01@gmail.com>
2026-09-10 15:02:18 +0530

    Revert "chore: remove stray temp file from root"
    This reverts commit 950b0c03e8bc6800e47d005731f98e1dc6b3a6b7.

--- apps/web/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6EYl/Ez09zln29NdOl1cTDHjYWbmRi2yOaFhiJk6gZE=\"\n}"
--- apps/web/.next/dev/static/chunks/apps_web_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/apps_web_app_globals_1lkgh9-.css","static/chunks/1-hf_next_dist_1j517xa._.js","static/chunks/_0jvstk8._.js"],
-    source: "entry"
-});
